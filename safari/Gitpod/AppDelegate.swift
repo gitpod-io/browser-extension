c1ea95635b5fe5adf646b5b5dcd07c662d25f131
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-08-30 20:34:11 +1000

    add branding

---
+++ safari/Gitpod/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Gitpod
+//
+//  Created by Geoffrey Huntley on 29/8/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
