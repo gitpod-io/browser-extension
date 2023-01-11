cb85e9a90704e609a2713da602640892d8cd388e
Filip Troníček <filip.tronicek@seznam.cz>
2023-01-11 22:54:41 +0100

    Revert "Use `executeScript` instead of `insertCSS`"
    This reverts commit 42ac0d6ceed41b2e47cbfa0a85730a94aa817510.

--- src/background.ts
+++ src/background.ts
@@ -2,19 +2,11 @@ const browser = require("webextension-polyfill");
 import { ConfigProvider, DEFAULT_CONFIG } from "./config";
 import { openLink } from "./utils";
 
-async function gitpodifyCurrentTab(tab) {
+async function gitpodifyCurrentTab() {
   try {
     // add a dummy div element to indicate that gitpodify.bundle.js was injected by a user click on the gitpod icon
-    browser.scripting.executeScript({
-      target: {
-        tabId: tab.id
-      },
-      func: () => {
-        const extensionIconClickedElement = document.createElement("div");
-        extensionIconClickedElement.id = "gitpod-extension-icon-clicked";
-        extensionIconClickedElement.style.display = "none";
-        document.body.appendChild(extensionIconClickedElement);
-      },
+    browser.scripting.insertCSS({
+      css: `document.body.innerHTML += '<div style=\"display: none;\" id=\"gitpod-extension-icon-clicked\"></div>'`,
     });
     browser.scripting.executeScript({
       files: ["dist/bundles/gitpodify.bundle.js"],
@@ -34,10 +26,9 @@ browser.action.onClicked.addListener(gitpodifyCurrentTab);
 
 browser.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install") {
-    openLink("https://www.gitpod.io/extension-activation?track=true");
+   openLink("https://www.gitpod.io/extension-activation?track=true");
   }
 });
 browser.runtime.setUninstallURL(
   "https://www.gitpod.io/extension-uninstall?track=true"
 );
-
