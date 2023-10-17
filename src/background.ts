4e651bdef8b4939548f7348ae73f21a71d9c1671
Filip Troníček <filip.tronicek@seznam.cz>
2023-10-17 18:53:28 +0200

    Remove debug for install and uninstall redirects

--- src/background.ts
+++ src/background.ts
@@ -6,9 +6,9 @@ addDomainPermissionToggle();
 
 browser.runtime.onInstalled.addListener((details) => {
     if (details.reason === "install") {
-        browser.tabs.create({ url: "https://www.gitpod.io/#extension-activation?track=true" });
+        browser.tabs.create({ url: "https://www.gitpod.io/extension-activation?track=true" });
     }
 });
-browser.runtime.setUninstallURL("https://www.gitpod.io/#extension-uninstall?track=true");
+browser.runtime.setUninstallURL("https://www.gitpod.io/extension-uninstall?track=true");
 
 export { }
