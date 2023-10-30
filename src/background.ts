97918d52702ce339a5af77f9f3e280a260bf8815
Filip Troníček <filip.tronicek@seznam.cz>
2023-10-30 18:18:26 +0100

    Universal package (MV2 for FF and MV3 for Chrome)

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,15 @@
 import { browser } from "webextension-polyfill-ts";
 import 'webext-dynamic-content-scripts';
-import addDomainPermissionToggle from "~utils/domain-accept";
+import addDomainPermissionToggleMv2 from "webext-domain-permission-toggle";
+import addDomainPermissionToggleMv3 from "~utils/domain-accept";
 
-addDomainPermissionToggle();
+(async () => {
+    if (browser.runtime.getManifest().manifest_version === 2) {
+        addDomainPermissionToggleMv2();
+    } else {
+        addDomainPermissionToggleMv3();
+    }
+})();
 
 browser.runtime.onInstalled.addListener((details) => {
     if (details.reason === "install") {
