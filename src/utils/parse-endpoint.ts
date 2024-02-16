5652f4c1f1b71e59aa1b0ac1e9a2ec58cea2b9bd
Filip Troníček <filip@gitpod.io>
2024-02-16 12:59:18 +0100

    Fix non-localhost hosts
    Localhost worked but broke everything else 🤷‍♂️

--- src/utils/parse-endpoint.ts
+++ src/utils/parse-endpoint.ts
@@ -3,7 +3,9 @@ import isUrl from "validator/es/lib/isURL";
 export const parseEndpoint = (input: string): string => {
     let url: URL;
 
-    if (isUrl(input, { require_protocol: true, protocols: ["https", "http"], host_whitelist: ["localhost"] })) {
+    if (isUrl(input, { require_protocol: true, protocols: ["https"] })) {
+        url = new URL(input);
+    } else if (isUrl(input, { require_protocol: true, protocols: ["https", "http"], host_whitelist: ["localhost"] })) {
         url = new URL(input);
     } else if (isUrl(input, { require_protocol: false, protocols: ["https"] })) {
         url = new URL(`https://${input}`);
