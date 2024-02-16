d668f41f27236b57bc1c7437312cfa51b86862bb
Filip Troníček <filip@gitpod.io>
2024-02-16 10:59:55 +0100

    Allow localhost URLs for testing

--- src/utils/parse-endpoint.ts
+++ src/utils/parse-endpoint.ts
@@ -3,10 +3,10 @@ import isUrl from "validator/es/lib/isURL"
 export const parseEndpoint = (input: string): string => {
   let url: URL
 
-  if (isUrl(input, { require_protocol: true, protocols: ["https"] })) {
+  if (isUrl(input, { require_protocol: true, protocols: ["https", "http"], host_whitelist: ["localhost"] })) {
     url = new URL(input)
   } else if (isUrl(input, { require_protocol: false, protocols: ["https"] })) {
-    url = new URL(`https://${input}`)
+    url = new URL(`https://${input}`);
   } else {
     throw new TypeError(`Invalid URL: ${input}`)
   }
