f0cc8d387331f5cf3dc57ca16a1ebb9419792a68
Filip Troníček <filip@gitpod.io>
2024-10-17 13:08:23 +0200

    Fix GitHub selector (#165)

--- src/button/button-contributions.ts
+++ src/button/button-contributions.ts
@@ -269,7 +269,7 @@ export const buttonContributions: ButtonContributionParams[] = [
             "https://github.com/svenefftinge/browser-extension-test",
             "https://github.com/svenefftinge/browser-extension-test/tree/my-branch",
         ],
-        selector: `xpath://*[contains(@id, 'repo-content-')]/div/div/div[2]/div[1]/react-partial/div/div/div[2]/div[2]`,
+        selector: `xpath://*[contains(@id, 'repo-content-')]/div/div/div/div[1]/react-partial/div/div/div[2]/div[2]`,
         containerElement: createElement("div", {}),
         additionalClassNames: ["medium"],
         application: "github",
