b3fdc65c6ec7894809d05d7fe4e6b8e2c562828d
Filip Troníček <29888641+filiptronicek@users.noreply.github.com>
2025-01-27 14:45:24 +0100

    Fix tests

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,4 +5,14 @@ const { join } = require("path");
  */
 module.exports = {
     cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+    browserRevision: "130.0.6723.58",
+    defaultLaunchOptions: {
+        headless: "new",
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
+        ]
+    }
 };
